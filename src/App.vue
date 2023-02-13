68ad0e1b4f870015d455dad54ca819e4666853da
Surya Karthik <90426887+surya7k@users.noreply.github.com>
2023-02-12 22:44:49 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,112 +10,85 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.card{
-  border-radius: 24px;
-  border: 3px solid #041E42;
-  padding: 8px;
+.wrapper {
   width: 400px;
+  border: 2px solid black;
+  display: inline-flex;
+}
+
+.image {
+  width: 400px;
+}
+
+.header {
+  text-align: center;
+  font-weight: bold;
+  font-size: 2rem;
+}
+
+.header h3:hover {
+  font-style: italic;
+  font-size: 1.1em;
+}
+
+.header h3,
+.header h4 {
+  transition: .3s ease-in-out all;
+  margin: 16px;
+  font-style: normal;
+}
+
+.buttons button:focus,
+.buttons button:hover {
+  background-color: rgba(200,0,50,.5);
+}
+
+.buttons button:active {
+  background-color: rgba(50,0,200,.5);
+}
+
+.buttons {
   display: block;
-  margin: auto;
-  position: relative;
-  overflow: hidden;
-  box-shadow: 0px 8px 24px #999;
-  background-color: lightgrey;
-  text-align: center;
-  font-family: Arial, Helvetica, sans-serif;
 }
-.textbox{
-  font-size: 15px;
-  margin-left: 112px;
-  text-align: right;
-  border-radius: 24px;
+
+button {
+  padding: 12px;
+  font-size: 32px;
 }
-.description{
-  text-shadow: -1px 1px 2px #FFFFFF,
-				  1px 1px 2px #FFFFFF,
-				  1px -1px 0 #FFFFFF,
-				  -1px -1px 0 #FFFFFF;
+
+details {
+  margin-left: 24px;
+  padding: 10px;
 }
-.haxbtn{
-  display:inline-block;
-  text-align: center;
-  color: white;
-  background-color: #041E42;
-  border-radius: 5px;
+.details summary {
   font-size: 20px;
-  width: 120px;
-  height: 32px;
-  box-shadow: 0px 10px 24px #999;
-  border: 1px solid #041E42;
-  visibility: visible;
-  padding-top: 10px;
-  margin-right: 32px;
+  font-weight: bold;
 }
-.haxbtn:hover{
-  color: #e0e0e0;
-  background-color: #07377a;
-  box-shadow: 0px 10px 24px white;
-  border: 1px solid #07377a;
+
+
+@media only screen and (max-width: 1200px){
+  .wrapper {
+    background-color: pink;
+  }
 }
-.haxbtn:focus{
-  color: #e0e0e0;
-  background-color: #07377a;
-  box-shadow: 0px 10px 24px white;
-  border: 1px solid #07377a;
+@media only screen and (max-width: 600px){
+  .wrapper {
+    background-color: purple;
+  }
 }
-.title{
-  color: white;
-  font-size: 24px;
-  text-shadow: -1px 1px 2px #000,
-				  1px 1px 2px #000,
-				  1px -1px 0 #000,
-				  -1px -1px 0 #000;
-}
-.Image{
-  width: 200px;
-  border-radius: 24px;
-  box-shadow: 0px 10px 24px #999;
-  float: left;
-}
-.outsideBtn{
-  color: white;
-  background-color: #041E42;
-  border-radius: 5px;
-  font-size: 20px;
-  width: 120px;
-  height: 50px;
-  box-shadow: 0px 10px 24px #999;
-  border: 1px solid #041E42;
-  margin-right: 32px;
-  margin-bottom: 16px;
-  display: inline-block;
-}
-.outsideBtn:hover{
-  color: #e0e0e0;
-  background-color: #07377a;
-  box-shadow: 0px 10px 24px white;
-  border: 1px solid #07377a;
-}
-.outsideBtn:focus{
-  color: #e0e0e0;
-  background-color: #07377a;
-  box-shadow: 0px 10px 24px white;
-  border: 1px solid #07377a;
-}
-.basic{
-  background-color: hotpink;
-}
-.hoverCard{
-  border: 3px solid purple;
-  box-shadow: 0px 8px 24px yellow;
+@media only screen and (max-width: 425px){
+  .wrapper {
+    font-weight: normal;
+  }
+  .wrapper .header h3 {
+    font-size: 12px;
+  }
+  .wrapper .header h4 {
+    font-size: 10px !important;
+  }
+  details {
+    display: none;
+  }
 }
 input:checked + label + .description { display:none; }
 </style>
